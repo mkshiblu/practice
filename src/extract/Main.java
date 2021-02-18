443d07d186187d49efb63f86f2c9750e0f1af993
Mosabbir Khan Shiblu <mkshiblu@gmail.com>
2021-02-18 17:09:23 -0500

    Extract createAddress from createAddresses

--- src/extract/Main.java
+++ src/extract/Main.java
@@ -3,6 +3,8 @@ package extract;
 import static java.lang.System.out;
 
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
@@ -11,19 +13,24 @@ public class Main {
 		Main obj = new Main() {
 			AtomicInteger PORTS;
 
-			Address[] createAddresses(int count) {
-				Address[] addresses = new Address[count];
+			List<Address> createAddresses(int count) {
+				List<Address> addresses = new ArrayList<>(count);
 
 				for (int i = 0; i < count; i++) {
-					try {
-						addresses[i] = new Address("127.0.0.1", PORTS.incrementAndGet());
-					} catch (UnknownHostException e) {
-						// e.printStackTrace();
-						out.println(e);
-					}
+					addresses.add(createAddress("127.0.0.1", PORTS.incrementAndGet()));
 				}
 
 				return addresses;
+			}
+
+			Address createAddress(String host, int port) {
+				try {
+					return new Address(host, port);
+				} catch (UnknownHostException e) {
+					out.println(e);
+				}
+
+				return null;
 			}
 		};
 	}
