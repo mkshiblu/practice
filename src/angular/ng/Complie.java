db2f4259fe7ae420e1f3f5acb849d2138831d5a2
mkshiblu <mkshiblu@gmail.com>
2021-12-07 17:15:08 -0500

    Extract setup contorller

--- src/angular/ng/Complie.java
+++ src/angular/ng/Complie.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.util.function.Consumer;
 import static angular.mocks.MockImport.createMap;
 import static angular.mocks.MockImport.$controller;
+import static angular.mocks.MockImport.controller;
 
 import angular.mocks.CompileContext;
 import angular.mocks.Controller;
@@ -24,53 +25,58 @@ public class Complie {
 
 	void nodeLinkFn(Object scope, HashMap<String, Directive> controllerDirectives, Directive newIsolateScopeDirective) {
 		Object i, isolateScope = null, controllerScope, transcludeFn = null;
-		Map<String, Controller> elementControllers;
+		Map<String, Controller> elementControllers = null;
 		Element $element = null;
 		Map<String, Character> attrs = null;
 
 		controllerScope = scope;
-
 		if (controllerDirectives != null) {
-			elementControllers = createMap();
-			for (var name : controllerDirectives.keySet()) {
-				var directive = controllerDirectives.get(name);
-				var locals = new Object() {
-					Object $scope = directive == newIsolateScopeDirective || directive.$$isolateScope == null
-							? isolateScope
-							: scope;
-					Object $$element = $element;
-					Object $attrs = attrs;
-					Object $transclude = transcludeFn;
-
-				};
-
-				var controllerConstructor = directive.controller;
-				if (controllerConstructor == '@') {
-					controllerConstructor = attrs.get(name);
-				}
-
-				var instance = $controller(controllerConstructor, locals, directive.controllerAs);
-
-				$element.data('$' + name + "Controller", instance);
-
-				var bindings = directive.$$bindings.bindToController;
-				var bindingInfo = initializeDirectiveBindings(controllerScope, attrs, instance, bindings, directive);
-				elementControllers.put(name, new Controller(instance, bindingInfo));
-			}
+			elementControllers = setupControllers($element, attrs, transcludeFn, controllerDirectives, isolateScope,
+					scope, newIsolateScopeDirective);
 		}
+		// Initialize bindToController bindings
+		for (var name : elementControllers.keySet()) {
+			var controllerDirective = controllerDirectives.get(name);
+			var controller = elementControllers.get(name);
+			var bindings = controllerDirective.$$bindings.bindToController;
 
+			controller.instance = controller();
+			$element.data('$' + controllerDirective.name + "Controller", controller.instance);
+			controller.bindingInfo = initializeDirectiveBindings(controllerScope, attrs, controller.instance, bindings,
+					controllerDirective);
+		}
 	}
 
-	Object getControllers(String directiveName, String require, String $element, Object elementControllers) {
-		Object value;
+	Map<String, Controller> setupControllers(Element $element, Map<String, Character> attrs, Object transcludeFn,
+			HashMap<String, Directive> controllerDirectives, Object isolateScope, Object scope,
+			Directive newIsolateScopeDirective) {
+		var elementControllers = createMap();
+		for (var controllerKey : controllerDirectives.keySet()) {
+			var directive = controllerDirectives.get(controllerKey);
+			var locals = new Object() {
+				Object $scope = directive == newIsolateScopeDirective || directive.$$isolateScope == null ? isolateScope
+						: scope;
+				Object $$element = $element;
+				Object $attrs = attrs;
+				Object $transclude = transcludeFn;
 
-		// Skipped Duplicated code, mock assignment
-		value = 1;
-		return value != null ? value : null;
+			};
+			var controller = directive.controller;
+			if (controller == '@') {
+				controller = attrs.get(directive.name);
+			}
+
+			var controllerInstance = $controller(controller, locals, true, directive.controllerAs);
+			elementControllers.put(directive.name, controllerInstance);
+			$element.data('$' + directive.name + "Controller", controllerInstance.instance);
+		}
+		return elementControllers;
 	}
+	
 
-	Object initializeDirectiveBindings(Object scope, Map<String, Character> attrs, Object destination, Object bindings,
-			Object directive) {
-		return new Object();
+
+	private Object initializeDirectiveBindings(Object controllerScope, Map<String, Character> attrs,
+			Controller instance, Object bindings, Directive controllerDirective) {
+		return null;
 	}
 }
