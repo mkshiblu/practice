cd0b49426b9d02a236f518a652f5737ea33fc400
mkshiblu <mkshiblu@gmail.com>
2022-03-21 19:53:55 -0400

    Extract method getCaretPositionFrom drawCaret

--- src/chartjs/ToolTip.java
+++ src/chartjs/ToolTip.java
@@ -3,9 +3,19 @@ package chartjs;
 public class ToolTip {
 	CTX ctx;
 
-	void drawCaret(int[] vm, int size, int opacity) {
+	void drawCaret(int[] vm, int size) {
 		int x = 1;
 		var ctx = this.ctx;
+		var caretPosition = getCaretPosition(vm, size);
+		
+		ctx.moveTo(caretPosition.x1, caretPosition.y1);
+		ctx.lineTo(caretPosition.x2, caretPosition.y2);
+		ctx.lineTo(caretPosition.x3, caretPosition.y3);
+		ctx.stroke();
+
+	}
+
+	public CaretPosition getCaretPosition(int[] vm, int size) {
 		int x1, x2, x3;
 		int y1, y2, y3;
 		var caretSize = vm.length;
@@ -25,11 +35,7 @@ public class ToolTip {
 		y2 = x2;
 		y3 = x3;
 
-		ctx.moveTo(x1, y1);
-		ctx.lineTo(x2, y2);
-		ctx.lineTo(x3, y3);
-		ctx.stroke();
-
+		return new CaretPosition(x1, x2, x3, y1, y2, y3);
 	}
 
 	void drawTitle() {
