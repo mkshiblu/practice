e6c793bfa5c087e41d1d32af87757411ca96b08b
mkshiblu <mkshiblu@gmail.com>
2022-06-27 16:32:11 -0400

    Extract function StringifyFn from Extract Args
    Original JavaScript commit https://github.com/angular/angular.js/commit/42e622b751d1ad1520b824473f9ad31e7efb75b3

--- src/extract/Injector.java
+++ src/extract/Injector.java
@@ -12,24 +12,32 @@ var FN_ARG = /^\s*(_?)(\S+?)\1\s*$/;
 var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
 var $injectorMinErr = minErr('$injector');
 
+function stringifyFn(fn) {
+  return Function.prototype.toString.call(fn);
+}
+
 function extractArgs(fn) {
-  var fnText = Function.prototype.toString.call(fn).replace(STRIP_COMMENTS, ''),
+  var fnText = stringifyFn(fn).replace(STRIP_COMMENTS, ''),
       args = fnText.match(ARROW_ARG) || fnText.match(FN_ARGS);
   return args;
 }
-*/
- 
-
+ */
 public class Injector {
 	String ARROW_ARG = "/^([^(]+?)=>/";
 	String FN_ARGS = "/^[^(]*\\(\\s*([^)]*)\\)/m";
 	String STRIP_COMMENTS = "/((\\/\\/.*$)|(\\/\\*[\\s\\S]*?\\*\\/))/mg";
+	
 
+	
+	StringVar stringifyFn(Object fn) {
+	  return Function.prototype.toString.call(fn);
+	}
+		
 	StringVar extractArgs(Object fn) {
-
-		StringVar fnText = Function.prototype.toString.call(fn).replace(STRIP_COMMENTS, ""),
+		
+		StringVar fnText =  stringifyFn(fn).replace(STRIP_COMMENTS, ""),
 				args = fnText.match(ARROW_ARG).match(FN_ARGS);
-
+		
 		return args;
 	}
 }
